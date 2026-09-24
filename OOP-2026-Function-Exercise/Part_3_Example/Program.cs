5fd28e71e30c3265e6525e939904bc741948f321
Amir <amgh@mmmi.sdu.dk>
2026-09-24 10:05:43 +0200

    Add Part 3 exercise

---
+++ OOP-2026-Function-Exercise/Part_3_Example/Program.cs
@@ -0,0 +1,15 @@
+﻿double CalculateDiscount(double price)
+{
+    double discount = price * 0.20;
+    double finalPrice = price - discount;
+
+    return finalPrice;
+}
+
+double price1 = CalculateDiscount(100);
+double price2 = CalculateDiscount(250);
+double price3 = CalculateDiscount(80);
+
+Console.WriteLine(price1);
+Console.WriteLine(price2);
+Console.WriteLine(price3);
