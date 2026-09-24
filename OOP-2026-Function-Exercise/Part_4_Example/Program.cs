aaa9f82da9ca9104d847e8ba3ce7c83287a875cd
Amir <amgh@mmmi.sdu.dk>
2026-09-24 10:19:04 +0200

    Add Part 4 example

---
+++ OOP-2026-Function-Exercise/Part_4_Example/Program.cs
@@ -0,0 +1,36 @@
+﻿int[] data = { 1, 3, 5, 8 };
+
+int maxD = MaxDiff(data);
+
+Console.WriteLine("The maximum difference is " + maxD);
+
+// Calculate the differences
+int MaxDiff(int[] array)
+{
+    int[] diffs = new int[array.Length - 1];
+
+    for (int i = 0; i < diffs.Length; i++)
+    {
+        diffs[i] = array[i + 1] - array[i];
+    }
+
+    return Max(diffs);
+}
+
+
+
+// Find the largest number.
+int Max(int[] array)
+{
+    int max = array[0];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+
+    return max;
+}
