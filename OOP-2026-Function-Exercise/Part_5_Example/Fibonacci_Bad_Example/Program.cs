667156d1f621d81ac8a45312179f7a6360542232
Amir <amgh@mmmi.sdu.dk>
2026-09-24 11:31:00 +0200

    Add Part 5 example

---
+++ OOP-2026-Function-Exercise/Part_5_Example/Fibonacci_Bad_Example/Program.cs
@@ -0,0 +1,20 @@
+﻿int Fib(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    else if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return Fib(n - 1) + Fib(n - 2);
+    }
+}
+
+for (int n = 0; n < 11; n++)
+{
+    Console.WriteLine(n + ": " + Fib(n));
+}
---
+++ OOP-2026-Function-Exercise/Part_5_Example/Recursion_Example/Program.cs
@@ -0,0 +1,13 @@
+﻿CountDown(3);
+
+void CountDown(int number)
+{
+    if (number == 0)
+    {
+        return;
+    }
+
+    Console.WriteLine(number);
+
+    CountDown(number - 1);
+}
