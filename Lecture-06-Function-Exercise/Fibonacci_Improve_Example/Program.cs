e5f04456bebd1daca209ce718c125f391880dcf0
Amir <amgh@mmmi.sdu.dk>
2026-09-25 14:41:16 +0200

    Improve Fibonacci example

---
+++ Lecture-06-Function-Exercise/Fibonacci_Improve_Example/Program.cs
@@ -0,0 +1,25 @@
+﻿int Fib(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+
+    int first = 0;
+    int second = 1;
+
+    for (int i = 2; i <= n; i++)
+    {
+        int next = first + second;
+
+        first = second;
+        second = next;
+    }
+
+    return second;
+}
+
+for (int i = 0; i < 11; i++)
+{
+    Console.WriteLine(i + ": " + Fib(i));
+}
